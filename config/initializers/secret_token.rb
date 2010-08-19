9c7266810bfdff74305bc0430c37e317a4999665
August <august@alum.rpi.edu>
2010-08-19 18:51:41 -0400

    Initial commit of Rails 3 FS Docs Branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlagshipDocs::Application.config.secret_token = 'b6f5d1f30da44b5b95039577a4be62b5cc9edd0f458554a9615b73ebb39e7d30be07be11c834bb734c84c484a44f312df991be38ab51c6b1dcc1ad57ffcc0e0b'
